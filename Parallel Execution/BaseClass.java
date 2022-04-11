970bfb43220a6b8f7f3e319e3c8fab554422c72e
fahadrahman18 <86140257+fahadrahman18@users.noreply.github.com>
2022-04-11 23:14:07 +0530

    Add files via upload

---
+++ Parallel Execution/BaseClass.java
@@ -0,0 +1,28 @@
+package week5.TestNG;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	public ChromeDriver driver;
+	
+	@BeforeMethod
+	public void precondition() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");	
+		//driver.manage().window().maximize();  //maximize window
+
+	}
+	
+	@AfterMethod
+	public void postcondition() {
+	
+		driver.quit();
+	}
+
+}
